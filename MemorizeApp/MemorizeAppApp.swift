2da408e8729911e17c8d93993ddec810ee0dd4ad
JiazhenLuo <luoj278@newschool.edu>
2025-04-10 02:42:22 -0400

    Initial Commit

---
+++ MemorizeApp/MemorizeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeAppApp.swift
+//  MemorizeApp
+//
+//  Created by Jiazhen Luo on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MemorizeAppTests/MemorizeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeAppTests.swift
+//  MemorizeAppTests
+//
+//  Created by Jiazhen Luo on 4/10/25.
+//
+
+import Testing
+@testable import MemorizeApp
+
+struct MemorizeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
